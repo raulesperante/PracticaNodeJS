ded61d444cb9e80a55d3bbb85b8a7f5d918127e4
Raul Gustavo <raulesperante@hotmail.com>
2020-02-02 17:37:07 -0300

    Models

---
+++ models/project.js
@@ -0,0 +1,17 @@
+'use strict'
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var ProjectSchema = Schema({
+    name: String,
+    descripcion: String,
+    categoria: String,
+    year: Number,
+    langs: [String]
+});
+
+// Parámetros tipo de entidad a guardar en db y Esquema
+// Mongoose por detrás toma el nombre 'Project', lo pasa a 
+// minúsculas y le pone una 's' al final
+module.exports = mongoose.model('Project', ProjectSchema);
